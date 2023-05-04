7e569131b476c77be9df883f68480fdb27cd95d0
Bob Bianchi <rbianx@gmail.com>
2023-05-04 16:06:11 -0400

    added validation

--- scripts.js
+++ scripts.js
@@ -1,17 +1,49 @@
-let guess;
+//declarations
+let answer;
+let maxNum;
+//promt to get Max Number
+answer = get_max("Enter the Max Number for our guessing game.");
 
+function get_max(prompt) {
+    let valid_input = false;
+    let num, input;
+
+    while (!valid_input) {
+        input = window.prompt(prompt);
+
+        maxNum = Number(input);
+
+        if (maxNum != NaN && maxNum > 0) {
+            valid_input = true;
+        }
+    }
+    console.log(`maxNum inputed was ${maxNum}`);
+    num = Math.floor(Math.random() * Math.round(maxNum)) + 1;
+    return num;
+}
+
+//console log target answer
+console.log(`The answer is ${answer}`);
+
+//on button click
 function gameStart() {
-    guess = Number(document.getElementById("numGuess").value);
-    console.log(guess);
-    if (isNaN(guess)) {
-        alert("Guess was not a number. Please resubmit");
-    } else if (guess % 1 != 0) {
-        alert("Guess was not a whole number. Please resubmit");
-    } else {
-        gameScript();
+    let guess = Number(document.getElementById("numGuess").value);
+    let feedback = document.getElementById("feedback");
+
+    if (guess > maxNum || guess < 1) {
+        feedback.innerHTML = "That number is not in range, try again.";
+    } else if (guess == answer) {
+        feedback.innerHTML = "Correct! Refresh to play again";
+    } else if (guess > answer) {
+        feedback.innerHTML = "Lower, try again.";
+    } else if (guess < answer) {
+        feedback.innerHTML = "Higher, try again.";
+    } else if (isNaN(guess)) {
+        feedback.innerHTML = "That is not a number!";
     }
 }
 
-function gameScript() {
-    let = 1;
+//initialize arrays
+function init_history() {
+    lorem;
 }
