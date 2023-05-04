e46ed4ca61ae29a713bdfff2cfd21a86f70e7bcc
Bob Bianchi <rbianx@gmail.com>
2023-05-04 12:08:39 -0400

    simplified

--- scripts.js
+++ scripts.js
@@ -1,9 +1,17 @@
-function gameStart(userInput) {
-    if (userInput <21 && userInput > -1) {
+let guess;
+
+function gameStart() {
+    guess = Number(document.getElementById("numGuess").value);
+    console.log(guess);
+    if (isNaN(guess)) {
+        alert("Guess was not a number. Please resubmit");
+    } else if (guess % 1 != 0) {
+        alert("Guess was not a whole number. Please resubmit");
+    } else {
         gameScript();
     }
 }
 
 function gameScript() {
-    let variableOne = 1;
+    let = 1;
 }
